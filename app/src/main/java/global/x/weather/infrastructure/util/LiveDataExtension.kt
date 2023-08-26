1c58e4c54929898b2aee85d0fc8b3a2128421390
bijaykumarpun <bijay.workplace@gmail.com>
2023-08-27 05:21:58 +0545

    chore: remove code comment

--- app/src/main/java/global/x/weather/infrastructure/util/LiveDataExtension.kt
+++ app/src/main/java/global/x/weather/infrastructure/util/LiveDataExtension.kt
@@ -8,17 +8,6 @@ fun LiveData<Boolean>.observeAsAction(
     action: () -> Unit,
     onActionResolved: () -> Unit
 ) {
-    // LiveData will only keep one subscription per-LifecycleOwner. If one is already present the
-    // new LiveData.observe() call will have no effect. See implementation of LiveData.observe() for
-    // reference. If action relies on variable parameters subsequent calls would be ignored and the
-    // old set of parameters will be used.
-    //
-    // The most common example is action using a CoroutineScope provided by a Composable. The
-    // CoroutineScope can change if the Composable is recreated (not just recomposed). By default
-    // the old scope would be used instead of the new one.
-    //
-    // To resolve this problem we remove previous observers on the provided LifecycleOwner is
-    // BEFORE calling LiveData.observe() to ensure a new subscription with new parameters.
     this.removeObservers(lifecycleOwner)
     this.observe(lifecycleOwner) { isActionRequested ->
         if (isActionRequested == true) {
@@ -33,17 +22,6 @@ fun <T> LiveData<T?>.observeAsAction(
     action: (T) -> Unit,
     onActionResolved: () -> Unit
 ) {
-    // LiveData will only keep one subscription per-LifecycleOwner. If one is already present the
-    // new LiveData.observe() call will have no effect. See implementation of LiveData.observe() for
-    // reference. If action relies on variable parameters subsequent calls would be ignored and the
-    // old set of parameters will be used.
-    //
-    // The most common example is action using a CoroutineScope provided by a Composable. The
-    // CoroutineScope can change if the Composable is recreated (not just recomposed). By default
-    // the old scope would be used instead of the new one.
-    //
-    // To resolve this problem we remove previous observers on the provided LifecycleOwner is
-    // BEFORE calling LiveData.observe() to ensure a new subscription with new parameters.
     this.removeObservers(lifecycleOwner)
     this.observe(lifecycleOwner) { actionParams ->
         if (actionParams != null) {
